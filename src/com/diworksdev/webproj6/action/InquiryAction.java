dfc065e3e525d90869853cc69ad252dce1ed4eaf
yunahamazawa <yunahamazawa821@gmail.com>
2025-02-10 15:21:35 +0900

    P40まで実行成功

---
+++ src/com/diworksdev/webproj6/action/InquiryAction.java
@@ -0,0 +1,11 @@
+package com.diworksdev.webproj6.action;
+
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class InquiryAction extends ActionSupport{
+	public String execute() {
+		return SUCCESS;
+		}
+}
